919fc558958acf23a8f2c85bd86dd9f21dbbbf5d
manish2524 <manish.s@rocketfuel.inc>
2026-01-28 21:30:02 +0530

    feat: added validation in call route

--- src/routes/CallRoutes.ts
+++ src/routes/CallRoutes.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import { createCall, listCalls, updateCallStatus } from '../controllers/CallController';
 import { authMiddleware } from '../middlewares/auth.middleware';
+import { validate } from '../middlewares/validation.middleware';
+import { createCallSchema, updateCallStatusSchema } from '../lib/validation';
 
 const router: Router = Router();
 
 router.use(authMiddleware);
 
-router.post('/', createCall);
+router.post('/', validate(createCallSchema), createCall);
 router.get('/', listCalls);
-router.patch('/:id/status', updateCallStatus);
+router.patch('/:id/status', validate(updateCallStatusSchema), updateCallStatus);
 
 export default router;
 
